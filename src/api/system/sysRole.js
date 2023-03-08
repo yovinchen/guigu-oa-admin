dc2af92884fea44fb5b6049f6e33c3efb32c8b43
yl <531591005@qq.com>
2023-03-08 23:11:59 +0800

    用户管理

--- src/api/system/sysRole.js
+++ src/api/system/sysRole.js
@@ -51,4 +51,17 @@ export default {
       data: idList
     })
   },
+  getRoles(adminId) {
+    return request({
+      url: `${api_name}/toAssign/${adminId}`,
+      method: 'get'
+    })
+  },
+  assignRoles(assginRoleVo) {
+    return request({
+      url: `${api_name}/doAssign`,
+      method: 'post',
+      data: assginRoleVo
+    })
+  }
 }
